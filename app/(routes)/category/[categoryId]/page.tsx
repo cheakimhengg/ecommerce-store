7a40315aa2fbdf65b53dfab10a80efedaa529a14
Chea KimHeng <cheakimhengg@gmail.com>
2024-03-04 16:46:17 +0700

    completed

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -8,6 +8,7 @@ import React from "react";
 import Filter from "./components/filter";
 import NoResults from "@/components/ui/no-result";
 import ProductCard from "@/components/ui/product-card";
+import MobileFilters from "./components/mobile-filters";
 
 export const revalidate = 0;
 
@@ -40,6 +41,7 @@ const Category: React.FC<CategoryPageProps> = async ({
         <Billboard data={category.billboard} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            <MobileFilters sizes={sizes} colors={colors} />
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
               <Filter valueKey="colorId" name="Colors" data={colors} />
