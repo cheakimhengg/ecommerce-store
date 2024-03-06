311777a0bd4a573e978f270dc7b40e9b9aa6eea2
Chea KimHeng <cheakimhengg@gmail.com>
2024-03-06 15:19:13 +0700

    ready to deply

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -1,28 +1,42 @@
 "use client";
 
-import React from "react";
 import axios from "axios";
 import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
-import toast from "react-hot-toast";
 
 import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
+import { toast } from "react-hot-toast";
 
 const Summary = () => {
   const searchParams = useSearchParams();
   const items = useCart((state) => state.items);
   const removeAll = useCart((state) => state.removeAll);
 
+  // useEffect(() => {
+  //   if (searchParams.get("success")) {
+  //     toast.success("Payment completed.");
+  //     removeAll();
+  //   }
+
+  //   if (searchParams.get("canceled")) {
+  //     toast.error("Something went wrong.");
+  //   }
+  // }, [searchParams, removeAll]);
+
   useEffect(() => {
+    console.log("useEffect triggered");
+
     if (searchParams.get("success")) {
-      toast.success("Payment successful! 🎉");
+      console.log("Success URL parameter found");
+      toast.success("Payment completed.");
       removeAll();
     }
 
     if (searchParams.get("canceled")) {
-      toast.error("Payment canceled 😢");
+      console.log("Canceled URL parameter found");
+      toast.error("Something went wrong.");
     }
   }, [searchParams, removeAll]);
 
@@ -42,19 +56,20 @@ const Summary = () => {
   };
 
   return (
-    <div
-      className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6
-    lg:col-span-5 lg:mt-0 lg:p-8"
-    >
-      <h2 className="text-lg font-medium text-gray-900">Summary</h2>
+    <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+      <h2 className="text-lg font-medium text-gray-900">Order summary</h2>
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
           <div className="text-base font-medium text-gray-900">Order total</div>
           <Currency value={totalPrice} />
         </div>
       </div>
-      <Button onClick={onCheckout} className="w-full mt-6">
-        Check Out
+      <Button
+        disabled={items.length === 0}
+        onClick={onCheckout}
+        className="w-full mt-6"
+      >
+        Checkout
       </Button>
     </div>
   );
